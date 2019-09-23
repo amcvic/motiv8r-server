3646d581a0c93c8918ebbed2805506c53fedab46
austin <austinmcvicker@twh-mbp.local>
2019-09-23 18:40:15 -0400

    sequelize associations added

--- db.js
+++ db.js
@@ -4,6 +4,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres'
 });
 
+sequelize.users = require('./models/user.js')(sequelize, Sequelize);
+sequelize.meetups = require('./models/meetup.js')(sequelize, Sequelize);
+sequelize.logs = require('./models/log.js')(sequelize, Sequelize);
+
+sequelize.users.hasMany(sequelize.meetups);
+sequelize.users.hasMany(sequelize.logs);
+
 sequelize.authenticate().then(
   function() {
     console.log('Connected to motiv8r postgres database');
